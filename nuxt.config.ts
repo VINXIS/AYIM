7bef12228f959b79db4f567e1041a649bf34498f
VINXIS <oykxf2@gmail.com>
2019-06-02 23:27:37 -0600

    I forgot the navigation stuff!!!! Now in its very own component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,12 @@ import NuxtConfiguration from "@nuxt/config"
 
 const config: NuxtConfiguration = {
     head: {
-        link: [
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Ubuntu' },
-        ],
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { name: 'keywords', content: 'osu, Corsace, mapping, osu!,rhythm game, ' },
+            { titleTemplate: '%s - Corsace' },
+
         ]
     },
     css: [
