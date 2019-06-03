1cbf049b547fcdd341dd2f2d64c455eb1a133877
VINXIS <oykxf2@gmail.com>
2019-06-02 23:28:02 -0600

    this doesn't work lol

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,6 @@ const config: NuxtConfiguration = {
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
             { name: 'keywords', content: 'osu, Corsace, mapping, osu!,rhythm game, ' },
-            { titleTemplate: '%s - Corsace' },
-
         ]
     },
     css: [
