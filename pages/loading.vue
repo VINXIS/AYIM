4f8af310440b721f17fa165f4308f54c6280ee58
VINXIS <oykxf2@gmail.com>
2019-06-07 01:06:41 -0600

    this loading stuff is gficing me aids

--- pages/loading.vue
+++ pages/loading.vue
@@ -1,17 +1,35 @@
 <template>
     <div class="loading">
         <div></div>
-        <img class="loading" src="../CorsaceAssets/img/corsace.png">
+        <img src="../CorsaceAssets/img/corsace.png">
     </div>
 </template>
 
 <style>
+@keyframes load1 {
+    0% { opacity: 0%; }
+    12.5% { opacity: 100%; }
+    87.5% { opacity: 100%; }
+    100% { opacity: 0%; }
+}
+
 .loading {
     display: flex;
     align-items: center;
     justify-content: center;
+    position: relative;
+}
+
+.loading img {
+    width: 2.55vw;
 }
 
 .loading div:nth-child(1) {
+    position: absolute;
+    width: 2.55vw;
+    height: 2.55vw;
+    border-bottom: 0.3vw solid #fb2475;
+    border-radius: 50%;
+    animation: load1 8s linear infinite;
 }
 </style>
